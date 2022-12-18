321c8cb65a21a85fce0e07be35e153b8e0eafb63
Ashley Mojica <ashjmojica@gmail.com>
2022-12-17 20:28:28 -0600

    commit tree 5505438805c8c22d70914d0ed8da0d95e5e82c60

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']; 
+
+function destructivelyAppendCat(name) {
+    console.log(name)
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    console.log(name)
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    console.log(name)
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    console.log(name)
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.push(name)
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift(name)
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.pop(name)
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.shift(name)
+    return copyOfCats
+}
